7583262198e3c1f2c597addede804103b1aba4da
Neras <medianeras57@gmail.com>
2020-05-29 00:07:57 +0800

    add editor hooks

--- src/hooks/useEditor.ts
+++ src/hooks/useEditor.ts
@@ -1,4 +1,27 @@
+import { useState, useCallback } from 'react';
+
 const useEditor = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const handleChangeTitle = useCallback(title => {
+    setTitle(title);
+  }, [])
+
+  const handleChangeContent = useCallback(content => {
+    setContent(content)
+  }, []);
+
+  return {
+    data: {
+      title,
+      content
+    },
+    events: {
+      handleChangeContent,
+      handleChangeTitle
+    }
+  }
 
 }
 
